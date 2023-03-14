728f3e3747e487a9705fa0476aeea1d8c7f7618d
panmile089 <panmile.089@gmail.com>
2023-03-14 11:06:40 +0700

    test by ui 03

--- src/main/java/th/co/pentasol/pcs/authen/daoimpl/UserDaoImpl.java
+++ src/main/java/th/co/pentasol/pcs/authen/daoimpl/UserDaoImpl.java
@@ -36,7 +36,7 @@ public class UserDaoImpl implements UserDao {
     @Override
     public UserEntity findByUserName(String userName){
         StringBuilder sql = selectUserSql().append("AND mus.user_name = ? ");
-        System.out.println("----- test by ui 02 -----");
+        System.out.println("----- test by ui 02 -----     asdasdsa");
         try{
             return jdbcTemplate.queryForObject(sql.toString(), new BeanPropertyRowMapper<>(UserEntity.class), userName);
         }catch (EmptyResultDataAccessException ignored){
